87b4a8f93f9265bfb3e04723d98beb9d1fee488c
Student 6 <Student 6@10.8.33.102>
2017-01-12 12:17:44 -0500

    stuff

--- SimonAfsana/src/PartnerCodeInHerePlease/Button.java
+++ SimonAfsana/src/PartnerCodeInHerePlease/Button.java
@@ -1,11 +1,20 @@
 package PartnerCodeInHerePlease;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 
 import SimonAfsana.ButtonInterfaceAfsana;
+import gui.components.Action;
 import gui.components.Component;
 
 public class Button extends Component implements ButtonInterfaceAfsana {
+
+	private double cos;
+	private Color color;
+	private Color displayColor;
+	private double sin;
+	private Action action;
+	public boolean highlighted;
 
 	public Button(int x, int y, int w, int h) {
 		super(x, y, w, h);
@@ -14,20 +23,58 @@ public class Button extends Component implements ButtonInterfaceAfsana {
 
 	@Override
 	public void act() {
-		// TODO Auto-generated method stub
-
+		action.act();
 	}
 
 	@Override
-	public boolean isHovered(int arg0, int arg1) {
-		// TODO Auto-generated method stub
+	public boolean isHovered(int x, int y) {
 		return false;
 	}
 
 	@Override
 	public void update(Graphics2D g) {
-		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	public void setColor(Color color) {
+		this.color = color;
+		
+	}
+
+	@Override
+	public void setX(double cos) {
+		this.cos = cos;
+		
+	}
+
+	@Override
+	public void setY(double sin) {
+		this.sin = sin;
+		
+	}
+
+	@Override
+	public void setAction(Action action) {
+		this.action = action;
+		
+	}
+
+	@Override
+	public void highlight() {
+		if(color != null){
+			highlighted = true;
+			displayColor = color;
+			update();
+		}
+		
+	}
+
+	@Override
+	public void dim() {
+		color = Color.gray;
+		highlighted = false;
+		update();
 	}
 
 }
--- SimonAfsana/src/PartnerCodeInHerePlease/Progress.java
+++ SimonAfsana/src/PartnerCodeInHerePlease/Progress.java
@@ -6,16 +6,41 @@ import SimonAfsana.ProgressInterfaceAfsana;
 import gui.components.Component;
 
 public class Progress extends Component implements ProgressInterfaceAfsana {
+	
+	public static final int height = 120;
+	public static final int width = 120;
 
-	public Progress(int x, int y, int w, int h) {
-		super(x, y, w, h);
-		// TODO Auto-generated constructor stub
+	public boolean gameOver;
+	public String round;
+	public String sequence;
+	
+	public Progress() {
+		super(85, 85, width, height);
 	}
 
 	@Override
 	public void update(Graphics2D g) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public void gameOver(){
+		gameOver = true;
+	}
+
+	public void setRound(int roundNumber) {
+		round = "Round" + roundNumber;
+		roundNumber++;
+		update();
+		
+	}
+
+	public void setSequenceSize(int size) {
+		size = 0;
+		sequence = "Sequence Size:" + size;
+		size++;
+		update();
+		
 	}
 
 }
