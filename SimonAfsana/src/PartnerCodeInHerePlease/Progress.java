42ca83502233ea10ae93286bdec71eaa19545730
Jonathan Zheng <Jonathan Zheng@DESKTOP-IT3333U.home>
2017-01-13 03:14:29 -0500

    code

--- SimonAfsana/src/PartnerCodeInHerePlease/Progress.java
+++ SimonAfsana/src/PartnerCodeInHerePlease/Progress.java
@@ -10,15 +10,15 @@ import gui.components.Component;
 
 public class Progress extends Component implements ProgressInterfaceAfsana {
 	
-	public static final int height = 60;
-	public static final int width = 120;
+	public static final int HEIGHT = 60;
+	public static final int WIDTH = 120;
 
 	public boolean gameOver;
 	public String round;
 	public String sequence;
 	
 	public Progress() {
-		super(85, 85, width, height);
+		super(85, 85, WIDTH, HEIGHT);
 	}
 
 	@Override
@@ -27,16 +27,16 @@ public class Progress extends Component implements ProgressInterfaceAfsana {
 		FontMetrics fm = g.getFontMetrics();
 		if(gameOver){
 			g.setColor(Color.black);
-			g.fillRect(0, 0, width, height);
+			g.fillRect(0, 0, WIDTH, HEIGHT);
 			g.setColor(Color.white);
 			String gameOver = "Game Over!";
-			g.drawString(gameOver, width - fm.stringWidth(gameOver)/2, 0);
-			g.drawString(sequence,  width - fm.stringWidth(sequence)/2, 40);
+			g.drawString(gameOver, WIDTH - fm.stringWidth(gameOver)/2, 0);
+			g.drawString(sequence,  WIDTH - fm.stringWidth(sequence)/2, 40);
 			
 		}else{
 			if(round != null && sequence != null){
-				g.drawString(round, width - fm.stringWidth(round)/2, 20);
-				g.drawString(sequence, width - fm.stringWidth(sequence)/2, 20);
+				g.drawString(round, WIDTH - fm.stringWidth(round)/2, 20);
+				g.drawString(sequence, WIDTH - fm.stringWidth(sequence)/2, 40);
 			}
 		}
 
@@ -49,7 +49,6 @@ public class Progress extends Component implements ProgressInterfaceAfsana {
 
 	public void setRound(int roundNumber) {
 		round = "Round" + roundNumber;
-		roundNumber++;
 		update();
 		
 	}
@@ -57,7 +56,6 @@ public class Progress extends Component implements ProgressInterfaceAfsana {
 	public void setSequenceSize(int size) {
 		size = 0;
 		sequence = "Sequence Size:" + size;
-		size++;
 		update();
 		
 	}
